f5768d1841793d7dfa674627543a4975e596bb71
franklin-gedler <tec.franklin.gedler@gmail.com>
2021-08-26 19:07:18 -0300

    Update 26-08-2021

--- Scripts-Main/Prepare-Ubuntu.sh
+++ Scripts-Main/Prepare-Ubuntu.sh
@@ -273,14 +273,14 @@ install_18-previous(){
 }
 
 install_pulse(){	
-	#wget --no-check-certificate "https://onedrive.live.com/download?cid=3D090B7E2735BB01&resid=3D090B7E2735BB01%21108&authkey=AKG_w7donFTjcTQ" -O pulse-9.0R4.x86_64.deb 2>&1
-	#gdebi -n pulse-9.0R4.x86_64.deb
+	wget --no-check-certificate "https://onedrive.live.com/download?cid=3D090B7E2735BB01&resid=3D090B7E2735BB01%21108&authkey=AKG_w7donFTjcTQ" -O pulse-9.0R4.x86_64.deb 2>&1
+	gdebi -n pulse-9.0R4.x86_64.deb
 
-	GITHUB_API_TOKEN="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"
-	GH_ASSET="https://api.github.com/repos/franklin-gedler/Scripts-Ubuntu/releases/assets/43371663"
-	curl -LJO# -H "Authorization: token $GITHUB_API_TOKEN" -H "Accept: application/octet-stream" "$GH_ASSET"
+	#GITHUB_API_TOKEN="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"
+	#GH_ASSET="https://api.github.com/repos/franklin-gedler/Scripts-Ubuntu/releases/assets/43371663"
+	#curl -LJO# -H "Authorization: token $GITHUB_API_TOKEN" -H "Accept: application/octet-stream" "$GH_ASSET"
 
-	gdebi -n Pulse-9.1r11.0-64bit.deb
+	#gdebi -n Pulse-9.1r11.0-64bit.deb
 
 	if [[ $varusr ]]; then
 		mkdir -p /home/$varusr/.pulse_secure/pulse/
